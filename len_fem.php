b8b0ed69712ceac8baae6517e3775080f27aa3f8
lucastelnovo <lucastelnovo@hotmail.com>
2013-08-16 15:27:17 -0300

    Viernes me quiero ir

---
+++ len_fem.php
@@ -0,0 +1,20 @@
+          
+          <div class="row-fluid hero-unit" style="position: relative; width: 900px; left: 85px; height:240px;">
+          
+            <div class="span4">
+              <h2>Medias</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Bombachas</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+             <div class="span4">
+              <h2>Trajes de ba&ntilde;o</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+          </div><!--/row-->
+        
---
+++ len_masc.php
@@ -0,0 +1,19 @@
+          
+          <div class="row-fluid hero-unit" style="position: relative; width: 900px; left: 85px; height:240px;">
+          
+            <div class="span4">
+              <h2>Medias</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Calzoncillos & Boxers</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+             <div class="span4">
+              <h2>Camisetas</h2>
+              <img alt="" src="http://placehold.it/250x180"><br><br>
+              <p><a class="btn" href="#">Ver &raquo;</a></p>
+            </div><!--/span-->
+          </div><!--/row-->
